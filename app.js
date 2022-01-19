587651575e4a261f7075de9b03ab074991aa6d15
elexis <eebrown422@gmail.com>
2022-01-18 22:21:12 -0500

    push

---
+++ app.js
@@ -0,0 +1,38 @@
+function returnSum  (a, b) {
+
+  var sum = (a + b) ; 
+console.log(sum)
+
+}
+
+returnSum(7,12);
+
+function returnDifference ( c , d) {
+
+var difference = ( c - d);
+
+console.log(difference)
+
+
+
+}
+
+returnDifference(6, 3);
+
+function returnProduct  ( e, f) {
+
+var product = ( e * f);
+
+console.log(product)
+
+}
+returnProduct(3, 6);
+
+function returnQuotient ( g, h) {
+
+    var quotient = ( g / h)
+
+    console.log(quotient)
+}
+
+returnQuotient(6, 3);
